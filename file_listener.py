44cf016509be7327c568a55241e2c72b77351aed
nouhjr <nouhjr>
2024-09-09 15:12:55 +0300

    enhancing file listener functionality

--- file_listener.py
+++ file_listener.py
@@ -3,10 +3,26 @@ import os
 from dotenv import load_dotenv
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from stg_area import *
 
 #Loading secured variables from .env file
 load_dotenv()
 source_files_dir_path = os.environ.get('SOURCE_FILES_DIR')
+source_files_arch_dir_path = os.environ.get('SOURCE_FILES_ARCHIVE_DIR')
+reference_date = os.environ.get('REFERENCE_DATE')
+
+#Function to wait for new files.
+def listen_for_file():
+    observer = Observer()
+    event_handler = MyHandler()
+    observer.schedule(event_handler, path= source_files_dir_path, recursive=False)
+    observer.start()
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 #File Listener class
 class MyHandler(FileSystemEventHandler):
@@ -16,12 +32,15 @@ class MyHandler(FileSystemEventHandler):
         for root, dirs, files in os.walk(directory_path):
             for file in files:
                 file_path = os.path.join(root, file)
+                abs_file_path = os.path.abspath(file_path)
+                final_path = abs_file_path.replace("\\","/")
                 file_name, file_ext = os.path.splitext(file)
-                file_dict['file_name'] = file_name 
-                file_dict['file_format'] = file_ext[1:]
+                file_dict[final_path] = file_ext[1:]
+        #Creating new dir named with files process date to archive source files
+        os.makedirs(source_files_arch_dir_path+'/'+reference_date)        
         return file_dict
 
     #Function to wait for file occurrence event and apply actions to it.
     def on_created(self, event):
         if not event.is_directory:
-            MyHandler.store_file_names_with_format(source_files_dir_path)
+            get_files_info(MyHandler.store_file_names_with_format(source_files_dir_path))
--- main_script.py
+++ main_script.py
@@ -1,22 +1,27 @@
 #Importing used packages
-import os
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pathlib import Path
 from dotenv import load_dotenv
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 #Our Created script to watch for file occurrence event
 from file_listener import *
+from stg_area import *
 
 #Main entry for the script
 if __name__ == "__main__":
+    #Loading secured variables from .env file
+    load_dotenv()
+    source_files_dir_path = os.environ.get('SOURCE_FILES_DIR')
+
+    #Check if the directory contains files or not
+    is_empty = not any(Path(source_files_dir_path).iterdir())
     
-    #File Listener
-    observer = Observer()
-    event_handler = MyHandler()
-    observer.schedule(event_handler, path= source_files_dir_path, recursive=False)
-    observer.start()
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    #Listen for new files
+    if is_empty:
+        #File Listener
+        listen_for_file()
+    #Work on existing files and listen for new files
+    else:
+        get_files_info(MyHandler.store_file_names_with_format(source_files_dir_path))
+        listen_for_file()   
