9de29e530de5706e0c35a37b528c407a9e7de399
nouhjr <nouhjr>
2025-05-04 13:27:52 +0300

    connecting to GCP & Upload data to BigQuery

---
+++ gcp_connection.py
@@ -0,0 +1,61 @@
+#Importing used packages
+import os
+from dotenv import load_dotenv
+from create_logs import write_log
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+load_dotenv()
+service_account_file = os.environ.get('SERVICE_ACCOUNT_FILE')
+project_id = os.environ.get('PROJECT_ID')
+reference_date = os.environ.get("REFERENCE_DATE")
+
+def upload_dataframes_to_bigquery(data_set_id, data_frame, table_name, upload_type):
+    write_log("Connecting to GCP....", "INFO")
+    # Convert spark dataframe to pandas data frame to load it in big query.
+    pandas_df = data_frame.toPandas()
+    # Authenticate with service account
+    credentials = service_account.Credentials.from_service_account_file(
+        service_account_file,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+
+    # Initialize BigQuery client
+    client = bigquery.Client(credentials=credentials, project=project_id)
+
+    if upload_type == "stg":
+        # Delete existing data with the specific reference date
+        write_log(f"Running pre SQL statement....", "INFO")
+        try:
+            query = f"""
+                DELETE FROM `{project_id}.{data_set_id}.{table_name.lower()}`
+                WHERE REFERENCE_DATE = DATE('{reference_date}')
+            """
+            pre_sql_job_config = bigquery.QueryJobConfig()
+            delete_job = client.query(query, job_config=pre_sql_job_config)
+            delete_job.result()
+            write_log(f"Pre SQL statement finished successfully.", "INFO")
+        except Exception as e:
+            write_log(f"{str(e)}", "ERROR")
+            write_log(f"Application terminated.", "INFO")
+            raise
+
+    # Configure job for loading data
+    upload_job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True,
+        write_disposition=bigquery.WriteDisposition.WRITE_APPEND
+    )
+    write_log(f"Writing data from: {table_name}, to BigQuery table....", "INFO")
+    try:
+        table_ref = client.dataset(data_set_id).table(table_name.lower())
+        job = client.load_table_from_dataframe(pandas_df, table_ref, upload_job_config)
+        job.result()
+        write_log(f"Uploaded {len(pandas_df)} rows to table: {table_ref}", "INFO")
+    except Exception as e:
+        write_log(f"{str(e)}", "ERROR")
+        write_log(f"Application terminated.", "INFO")
+        raise    
+
+
